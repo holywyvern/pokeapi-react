669c8388044fdc1a209426635226bb77453b4743
Ramiro Rojo <ramiro.rojo.cretta@gmail.com>
2020-04-03 16:11:04 -0300

    Add locales and redux provider

--- src/components/Application/index.js
+++ src/components/Application/index.js
@@ -3,6 +3,8 @@ import { Provider as ReduxProvider } from "react-redux";
 import { createBrowserHistory } from "history";
 import configureStore from "@/state";
 
+import "./i18n";
+
 const history = createBrowserHistory();
 const store = configureStore(history);
 
