378cb2707f2b981fe0269d8ef5736f45ecaaba2e
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2020-04-05 20:05:25 +0000

    Deploy holywyvern/pokeapi-react to github.com/holywyvern/pokeapi-react.git:gh-pages

---
+++ precache-manifest.40d3f21688397bf1497cabe438e8e18e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1fe2104ff24bae0632864dcd77377bcb",
+    "url": "/pokeapi-react/index.html"
+  },
+  {
+    "revision": "ac27ace2850da22fabda",
+    "url": "/pokeapi-react/static/css/main.afcbe0d0.chunk.css"
+  },
+  {
+    "revision": "2cd6f516b924c44405c0",
+    "url": "/pokeapi-react/static/js/2.9095fc0e.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/pokeapi-react/static/js/2.9095fc0e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ac27ace2850da22fabda",
+    "url": "/pokeapi-react/static/js/main.ef2ade22.chunk.js"
+  },
+  {
+    "revision": "ab9f2d1aa32425bf49a9",
+    "url": "/pokeapi-react/static/js/runtime-main.fa285131.js"
+  }
+]);
